78229390c15b71ad90e6ddcb4c61183f11d3d5c2
slimen <mamislimenproxy@gmail.com>
2019-03-26 18:41:18 +0100

    Initial commit

---
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  children: PropTypes.node,
+};
+
+const defaultProps = {};
+
+class DefaultFooter extends Component {
+  render() {
+
+    // eslint-disable-next-line
+    const { children, ...attributes } = this.props;
+
+    return (
+      <React.Fragment>
+        <span><a href="https://esprit.tn">Angry Birds Team</a> &copy; 2019</span>
+        <span className="ml-auto">Powered by <a href="https://coreui.io/react">Esprit</a></span>
+      </React.Fragment>
+    );
+  }
+}
+
+DefaultFooter.propTypes = propTypes;
+DefaultFooter.defaultProps = defaultProps;
+
+export default DefaultFooter;
